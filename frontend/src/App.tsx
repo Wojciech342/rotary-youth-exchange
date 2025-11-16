a7e74ffa730fccb79df56cec9f624f2f12809c3b
Mykhailo Derkach <my.derkach@gmail.com>
2025-11-16 19:38:23 +0100

    feat: redefine app structure with url routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './hooks/auth';
+import ProtectedRoute from './components/protected-route';
 import styles from './styles/App.module.css';
 
 function App() {
   return (
+    <AuthProvider>
+      <Routes>
+        <Route path='/login' element={} />
 
+        <Route element={<ProtectedRoute />}>
+          <Route path='/' element={}>
+            <Route index element={} />
+            <Route path="current" element={} />
+            <Route path="archive" element={} />
+            <Route path="coordinators" element={} />
+          </Route>
+        </Route>
+      </Routes>
+    </AuthProvider>
   );
 }
 
