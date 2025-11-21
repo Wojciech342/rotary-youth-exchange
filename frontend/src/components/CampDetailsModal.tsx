0651c0ff17d5d88f5ba80bd3d48a8aa5fe636d9a
Mykhailo Derkach <my.derkach@gmail.com>
2025-11-21 16:31:01 +0100

    feat: remove uppercase for details title in camp modal

--- frontend/src/components/CampDetailsModal.tsx
+++ frontend/src/components/CampDetailsModal.tsx
@@ -18,35 +18,35 @@ const CampDetailsModal = ({ camp, onClose }: CampDetailsModalProps) => {
                 <div className={styles.detailsColumn}>
                     <div className={styles.details}>
                         <div className={styles.countryDetails}>
-                            <p className={styles.strong}>Country:</p>
+                            <p className={styles.strong}>Country</p>
                             <p className={`${styles.country} ${styles.detail}`}>{camp.country}</p>
                         </div>
                         <div className={styles.priceDetails}>
-                            <p className={styles.strong}>Price:</p>
+                            <p className={styles.strong}>Price</p>
                             <p className={`${styles.price} ${styles.detail}`}>{camp.price} USD</p>
                         </div>
                         <div className={styles.ageDetails}>
-                            <p className={styles.strong}>Age:</p>
+                            <p className={styles.strong}>Age</p>
                             <p className={`${styles.age} ${styles.detail}`}>{camp.age_min}-{camp.age_max}</p>
                         </div>
                         <div className={styles.dateDetails}>
-                            <p className={styles.strong}>Date:</p>
+                            <p className={styles.strong}>Date</p>
                             <p className={`${styles.date} ${styles.detail}`}>{formattedDate}</p>
                         </div>
                     </div>
 
                     <p className={styles.statusDetails}>
-                        <p className={styles.strong}>Status:</p>
+                        <p className={styles.strong}>Status</p>
                         <p className={`${styles.status} ${styles.detail}`}>{camp.status}</p>
                     </p>
 
                     <div className={styles.descriptionDetails}>
-                        <p className={styles.strong}>Description:</p>
+                        <p className={styles.strong}>Description</p>
                         <p className={styles.description}>{camp.description}</p>
                     </div>
 
                     <div className={styles.organizerDetails}>
-                        <p className={styles.strong}>Organizer:</p>
+                        <p className={styles.strong}>Organizer</p>
                         <p className={`${styles.organizer} ${styles.detail}`}>{camp.coordinator.name}</p>
                     </div>
                 </div>
